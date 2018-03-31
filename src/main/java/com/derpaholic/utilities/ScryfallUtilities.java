2e075312146d20dd6b98c655847594ead24b3bbc
Allen Conlon <allen.conlon.12@cnu.edu>
2018-03-31 14:20:20 -0400

    Removed silly print from Scryfall.

--- src/main/java/com/derpaholic/utilities/ScryfallUtilities.java
+++ src/main/java/com/derpaholic/utilities/ScryfallUtilities.java
@@ -17,7 +17,6 @@ public class ScryfallUtilities {
 
     private static JsonObject getJsonFromURL(String url) {
         try {
-            System.out.println(url);
             HttpURLConnection request = (HttpURLConnection) new URL(url).openConnection();
             request.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
             request.connect();
